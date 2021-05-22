f549df451e7ba1cfac3ac2de0913a0b86dd0f944
a.guerra <a.guerra@datys.cu>
2021-05-21 21:11:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/fizzbuzz', (req, res) => {
+    let result = ''
+    for (let index = 1; index < 101; index++) {
+        if(index%15==0)
+            result+="fizzbuzz\n"
+        else if(index%3==0)
+            result+="fizz\n"
+        else if(index%5==0)
+            result+="buzz\n"
+        else
+            result+=index+"\n"
+    }
+    res.send(result)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/fizzbuzz`)
+})
