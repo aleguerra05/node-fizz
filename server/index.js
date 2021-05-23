3e09f6adef29a022fba7353d5c13d49c2fe53e6c
Alejandro <aleguerra05@gmail.com>
2021-05-22 22:38:06 -0400

    - adding transparency - link to Github

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.use(function(req, res, next) {
   
     // respond with html page
     if (req.accepts('html')) {
-      res.render('404', { url: req.url });
+      res.render('404', {title:"404 Not Found" });
       return;
     }
 })
