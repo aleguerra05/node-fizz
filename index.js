62a4fdb326db86a591131d3e34b8561ff252d138
a.guerra <a.guerra@datys.cu>
2021-05-24 18:45:52 -0400

    - Removing unused packages - Change the url on the message when run

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var server = require('./server');
 var port = process.env.PORT || 8000;
 
 server.listen(port, function () {
-  console.log(`Example app listening at http://localhost:${port}/fizzbuzz`)
+  console.log(`Example app listening at http://localhost:${port}`)
 });
